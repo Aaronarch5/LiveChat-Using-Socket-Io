ee7576ab73ee4c34d6bcff0d21777d9e478c2c58
Aaron Archundia <63936853+Aaronarch5@users.noreply.github.com>
2024-08-13 01:57:12 -0600

    Dom Elements

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,7 @@
 const socket = io();
+//Dom Elements
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let button = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('action');
